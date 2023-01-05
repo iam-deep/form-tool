8da49c21d730ba04f87cfdc37c6dedd19edcbfeb
Biswadeep Sarkar <biswadeepoffice@gmail.com>
2023-01-05 20:55:20 +0530

    Configurable auth middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['prefix' => config('form-tool.adminURL'), 'middleware' => ['web', 'auth']], function () {
+Route::group(['prefix' => config('form-tool.adminURL'), 'middleware' => ['web', config('form-tool.auth.middleware', 'auth')]], function () {
     Route::post('form-tool/editor-upload', [Biswadeep\FormTool\Core\InputTypes\EditorType::class, 'uploadImage']);
 });
