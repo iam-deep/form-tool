d9c08c37442ab78781ef2d84a632a48a9f6397ea
Biswadeep Sarkar <iam-deep@users.noreply.github.com>
2023-10-25 07:18:58 +0000

    Apply fixes from StyleCI

--- src/Core/InputTypes/Common/ICustomType.php
+++ src/Core/InputTypes/Common/ICustomType.php
@@ -3,10 +3,10 @@
 namespace Deep\FormTool\Core\InputTypes\Common;
 
 /**
- * For Select
+ * For Select.
+ *
  * @method static \Deep\FormTool\Core\InputTypes\SelectType first(string $firstOption, $firstValue = '')
  */
-
 interface ICustomType
 {
     public function getHTML();
