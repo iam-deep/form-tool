4f613544aef4959851d12358c6d0b3297a5e54e8
Biswadeep Sarkar <biswadeepoffice@gmail.com>
2024-01-07 21:16:14 +0530

    Update file.php

--- src/views/form/input_types/file.php
+++ src/views/form/input_types/file.php
@@ -6,24 +6,22 @@ $maxHeight = config('form-tool.imageThumb.form.maxHeight', '80px');
 if ($input->type == 'single') { ?>
 
     <div style="display:flex;">
-        <div style="max-width:<?php echo $maxWidth; ?>" id="preview-<?php echo $input->column; ?>"
+        <div style="max-width:<?php echo $maxWidth; ?>;display: flex;align-items: center;" id="preview-<?php echo $input->column; ?>"
             data-isimage="<?php echo $input->isImageField ? 1 : 0; ?>">
             <a href="<?php echo $input->value; ?>" target="_blank" <?php if (! $input->rawValue) { ?> style="pointer-events:none;" <?php } ?>>
                 <img src="<?php echo $input->imageCache; ?>" id="image-<?php echo $input->column; ?>"
-                    class="img-thumbnail" style="max-height:<?php echo $maxHeight; ?>;max-width:<?php echo $maxWidth; ?>;<?php if (! $input->isImage && $input->rawValue) {
-                        ?> display:none; <?php } ?>" alt="Image">
-                <i class="<?php echo $input->icon; ?> fa-5x" style="color:#4367A5;<?php if ($input->isImage || ! $input->rawValue) {
-                    ?> display:none; <?php } ?>"></i>
+                    class="img-thumbnail" style="max-height:<?php echo $maxHeight; ?>;max-width:<?php echo $maxWidth; ?>;
+                    <?php if (! $input->isImage && $input->rawValue) {?> display:none; <?php } ?>" alt="Image">
+                    
+                <i class="<?php echo $input->icon; ?> fa-5x" style="color:#4367A5;<?php if ($input->isImage || ! $input->rawValue) {?> display:none; <?php } ?>"></i>
             </a>
         </div>
         <div style="margin-left:15px;">
-            <label style="font-weight:550;margin-bottom:0;cursor: pointer;"
-                for="<?php echo $input->column; ?>">
+            <label style="font-weight:550;margin-bottom:0;cursor: pointer;" for="<?php echo $input->column; ?>">
                 Browse your file
             </label>
-            <input type="file" class="<?php echo $input->classes; ?>" id="<?php echo $input->column; ?>"
-                name="<?php echo $input->column; ?>" accept="<?php echo $input->accept; ?>"
-                style="display:none;" <?php echo $input->raw; ?> />
+            <input type="file" class="<?php echo $input->classes; ?>" id="<?php echo $input->column; ?>" name="<?php echo $input->column; ?>"
+                accept="<?php echo $input->accept; ?>" style="display:none;" <?php echo $input->raw; ?> />
 
             <div style="color:#76787a;font-size:13px;">
                 <?php if ($input->isImageField) { ?>
@@ -34,22 +32,18 @@ if ($input->type == 'single') { ?>
             </div>
 
             <div style="margin-top: 5px;">
-                <div id="hasFile-<?php echo $input->column; ?>" style="<?php if (! $input->rawValue) {
-                    ?> display:none <?php } ?>">
+                <div id="hasFile-<?php echo $input->column; ?>" style="<?php if (! $input->rawValue) { ?> display:none <?php } ?>">
                     <p style="margin-bottom:0;color:#00a65a;">
                         <strong id="filename-<?php echo $input->column; ?>"></strong>
                     </p>
-                    <label style="cursor: pointer;color:#337ab7;" class="text-bold"
-                        for="<?php echo $input->column; ?>">
+                    <label style="cursor: pointer;color:#337ab7;" class="text-bold" for="<?php echo $input->column; ?>">
                         Replace
                     </label> &nbsp;
-                    <a href="javascript:;" id="remove-<?php echo $input->column; ?>"
-                        class="text-danger text-bold">Remove</a>
-                    <input type="hidden" name="<?php echo $input->column; ?>"
-                        id="value-<?php echo $input->column; ?>" value="<?php echo $input->rawValue; ?>">
+                    <a href="javascript:;" id="remove-<?php echo $input->column; ?>" class="text-danger text-bold">Remove</a>
+                    <input type="hidden" name="<?php echo $input->column; ?>" id="value-<?php echo $input->column; ?>" value="<?php echo $input->rawValue; ?>">
                 </div>
-                <label id="noFile-<?php echo $input->column; ?>" style="cursor: pointer;<?php if ($input->rawValue) {
-                    ?> display:none <?php } ?>" class="text-primary text-bold" for="<?php echo $input->column; ?>">
+                <label id="noFile-<?php echo $input->column; ?>" style="cursor: pointer;<?php if ($input->rawValue) {?> display:none <?php } ?>"
+                    class="text-primary text-bold" for="<?php echo $input->column; ?>">
                     Upload
                 </label>
             </div>
@@ -111,8 +105,7 @@ if ($input->type == 'single') { ?>
     <div class="row">
         <div class="col-sm-3">
             <input type="file" class="<?php echo $input->classes; ?>" id="<?php echo $input->id; ?>"
-                name="<?php echo $input->name; ?>" accept="<?php echo $input->accept; ?>"
-                <?php echo $input->raw; ?> />
+                name="<?php echo $input->name; ?>" accept="<?php echo $input->accept; ?>" <?php echo $input->raw; ?> />
         </div>
 
         <?php if ($input->rawValue) {
@@ -123,8 +116,7 @@ if ($input->type == 'single') { ?>
 
             if ($input->isImage) {
                 $image = $input->imageCache;
-                $file = '<img src="'.$input->imageCache.'" class="img-thumbnail"
-                    style="max-height:'.$maxHeight.';max-width:'.$maxWidth.';" alt="Image">';
+                $file = '<img src="'.$input->imageCache.'" class="img-thumbnail" style="max-height:'.$maxHeight.';max-width:'.$maxWidth.';" alt="Image">';
             } else {
                 $file = '<i class="'.$input->icon.' fa-5x"></i>';
             } ?>
