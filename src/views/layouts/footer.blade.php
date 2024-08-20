a4d4d8abd840f113d285083b45e821252e58581a
Biswadeep Sarkar <biswadeepoffice@gmail.com>
2024-08-20 23:09:57 +0530

    Code Organized

--- src/views/layouts/footer.blade.php
+++ src/views/layouts/footer.blade.php
@@ -1,11 +1,11 @@
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-      Processed in {{ round(microtime(true) - LARAVEL_START, 2) }} secs
-    </div>
-    <!-- Default to the left -->
-    <strong>
-        Copyright &copy; {{ date('Y') }} <a href="/" target="_blank">{{ config('app.name', 'Admin') }}</a>.
-    </strong> All rights reserved.
-  </footer>
+<!-- Main Footer -->
+<footer class="main-footer">
+  <!-- To the right -->
+  <div class="pull-right hidden-xs">
+    Processed in {{ round(microtime(true) - LARAVEL_START, 2) }} secs
+  </div>
+  <!-- Default to the left -->
+  <strong>
+      Copyright &copy; {{ date('Y') }} <a href="{{ url('/') }}" target="_blank">{{ config('app.name', 'Admin') }}</a>.
+  </strong> All rights reserved.
+</footer>
--- src/views/layouts/header.blade.php
+++ src/views/layouts/header.blade.php
@@ -1,69 +1,69 @@
-  <!-- Main Header -->
-  <header class="main-header">
+<!-- Main Header -->
+<header class="main-header">
 
-    <!-- Logo -->
-    <a href="{{ url(config('form-tool.adminURL') . '/dashboard') }}" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini">{{ substr(config('app.name', 'Admin'), 0, 3) }}</span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg">{{ config('app.name', 'Admin') }}</span>
+  <!-- Logo -->
+  <a href="{{ url(config('form-tool.adminURL') . '/dashboard') }}" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini">{{ substr(config('app.name', 'Admin'), 0, 3) }}</span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg">{{ config('app.name', 'Admin') }}</span>
+  </a>
+
+  <!-- Header Navbar -->
+  <nav class="navbar navbar-static-top" role="navigation">
+    <!-- Sidebar toggle button-->
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+      <span class="sr-only">Toggle navigation</span>
     </a>
-
-    <!-- Header Navbar -->
-    <nav class="navbar navbar-static-top" role="navigation">
-      <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-        <span class="sr-only">Toggle navigation</span>
-      </a>
-      <!-- Navbar Right Menu -->
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          @if ('website' == config('app.type'))
-            <li>
-              <a href="{{ url('/') }}" target="_blank" title="View Website" data-toggle="tooltip">
-                <i class="fa fa-globe"></i>
-              </a>
-            </li>
-          @endif
-
-          <!-- User Account Menu -->
-          <li class="dropdown user user-menu">
-            <!-- Menu Toggle Button -->
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <!-- The user image in the navbar-->
-              {{-- <img src="/assets/{{ ADMIN_DIR }}/dist/img/avatar.png" class="user-image" alt="User"> --}}
-              <!-- hidden-xs hides the username on small devices so only the image appears. -->
-              <span class="hidden-xs">{{ auth()->user()->name ?? null }}</span>
-              &nbsp; <i class="fa fa-caret-down"></i>
+    <!-- Navbar Right Menu -->
+    <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+        @if ('website' == config('app.type'))
+          <li>
+            <a href="{{ url('/') }}" target="_blank" title="View Website" data-toggle="tooltip">
+              <i class="fa fa-globe"></i>
             </a>
-            <ul class="dropdown-menu">
-              <!-- The user image in the menu -->
-              <li class="user-header">
-                <img src="{{ asset('assets/form-tool/dist/img/avatar.png') }}" class="img-circle" alt="User Image">
-
-                <p>
-                  {{ auth()->user()->name ?? null }}
-                </p>
-              </li>
-              <li class="user-footer">
-                <div class="pull-left">
-                  <a href="{{ url(config('form-tool.adminURL') . '/change-password') }}"
-                    class="btn btn-default btn-flat">Password</a>
-                </div>
-                <div class="pull-right">
-                  {{-- <a href="{{ url(config('form-tool.adminURL') . '/logout') }}" class="btn btn-default btn-flat">Sign out</a> --}}
-                  <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();"
-                        class="btn btn-default btn-flat">
-                        {{ __('Log Out') }}
-                    </a>
-                  </form>
-                </div>
-              </li>
-            </ul>
           </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+        @endif
+
+        <!-- User Account Menu -->
+        <li class="dropdown user user-menu">
+          <!-- Menu Toggle Button -->
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <!-- The user image in the navbar-->
+            {{-- <img src="/assets/{{ ADMIN_DIR }}/dist/img/avatar.png" class="user-image" alt="User"> --}}
+            <!-- hidden-xs hides the username on small devices so only the image appears. -->
+            <span class="hidden-xs">{{ auth()->user()->name ?? null }}</span>
+            &nbsp; <i class="fa fa-caret-down"></i>
+          </a>
+          <ul class="dropdown-menu">
+            <!-- The user image in the menu -->
+            <li class="user-header">
+              <img src="{{ asset('assets/form-tool/dist/img/avatar.png') }}" class="img-circle" alt="User Image">
+
+              <p>
+                {{ auth()->user()->name ?? null }}
+              </p>
+            </li>
+            <li class="user-footer">
+              <div class="pull-left">
+                <a href="{{ url(config('form-tool.adminURL') . '/change-password') }}"
+                  class="btn btn-default btn-flat">Password</a>
+              </div>
+              <div class="pull-right">
+                {{-- <a href="{{ url(config('form-tool.adminURL') . '/logout') }}" class="btn btn-default btn-flat">Sign out</a> --}}
+                <form method="POST" action="{{ route('logout') }}">
+                  @csrf
+                  <a href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();"
+                      class="btn btn-default btn-flat">
+                      {{ __('Log Out') }}
+                  </a>
+                </form>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
